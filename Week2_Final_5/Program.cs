1126aa6d1bb0f00dfee1160f70c6727953cc5083
cang8 <0.gizemgunes@gmail.com>
2024-10-07 21:34:01 +0300

    added-5

---
+++ Week2_Final_5/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Lütfen yaşınızı giriniz:");
+int age = int.Parse(Console.ReadLine());
+
+// 18'den büyükse "+" küçükse "-" yazdırma
+if (age > 18)
+{
+    Console.WriteLine("+");
+}
+else if (age < 18)
+{
+    Console.WriteLine("-");
+}
+else
+{
+    Console.WriteLine(":)");
+}
