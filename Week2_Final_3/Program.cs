7a15b97f4c6a92ff2b639d76ec0ca808fd36aa29
cang8 <0.gizemgunes@gmail.com>
2024-10-07 21:14:06 +0300

    added-3

---
+++ Week2_Final_3/Program.cs
@@ -0,0 +1,5 @@
+﻿Random random = new Random(); // random sınıfından instance alma
+
+int randomValue = random.Next(); // random sayı oluşturma
+
+Console.WriteLine("Uretilen rastgele sayı: "+ randomValue); // random sayıyı ekrana yazdırma
