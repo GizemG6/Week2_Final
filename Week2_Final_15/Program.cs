5f3a899477c576b61560b74506b7b38b59c1fcc6
cang8 <0.gizemgunes@gmail.com>
2024-10-08 13:29:52 +0300

    added-15

---
+++ Week2_Final_15/Program.cs
@@ -0,0 +1,12 @@
+﻿static double Distance(double speed, double time) // hizi(km/saat) ve zamani(saat) alip yolu(km) donduren metot
+{
+    return speed * time;
+}
+
+Console.WriteLine("Hizi (km/saat cinsinden) giriniz:");
+double speed = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Zamani (saat cinsinden) giriniz:");
+double time = Convert.ToDouble(Console.ReadLine());
+
+double distance = Distance(speed, time);
+Console.WriteLine($"Gidilen yol : {distance} kilometre");
