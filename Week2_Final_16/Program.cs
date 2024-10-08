3ce5fb2f6c2a4efc16713fe6fded8ad32df2b4c8
cang8 <0.gizemgunes@gmail.com>
2024-10-08 13:37:13 +0300

    added-16

---
+++ Week2_Final_16/Program.cs
@@ -0,0 +1,10 @@
+﻿static double CalculateCircleArea(double radius)
+{
+    return Math.PI * Math.Pow(radius, 2);
+}
+
+Console.WriteLine("Dairenin yaricapini giriniz:");
+double radius = Convert.ToDouble(Console.ReadLine());
+
+double area = CalculateCircleArea(radius);
+Console.WriteLine($"Yaricapini girdiginiz dairenin alanı: {area} birim kare");
