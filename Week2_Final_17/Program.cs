739f0c0b224b9d91e5454f3515c749eb922b1659
cang8 <0.gizemgunes@gmail.com>
2024-10-08 13:45:43 +0300

    added-17

---
+++ Week2_Final_17/Program.cs
@@ -0,0 +1,17 @@
+﻿static string UpperCase(string sentence)
+{
+    return sentence.ToUpper();
+}
+
+static string LowerCase(string sentence)
+{
+    return sentence.ToLower();
+}
+
+Console.WriteLine("Bir cumle giriniz:");
+string sentence = Console.ReadLine();
+
+string upperCase = UpperCase(sentence);
+string lowerCase = LowerCase(sentence);
+
+Console.WriteLine($"Girdiginiz cumlenin buyuk harflerle yazilisi: {upperCase}\nve kucuk harflerle yazilisi: {lowerCase}");
